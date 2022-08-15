e69d902008231da5059e53b68ae6c81372dbd5b6
felipemoralesquerol <felipe.morales.querol@gmail.com>
2022-08-15 16:38:39 -0300

    Agregado de mejoras visuales e invocaciones a endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//let URL = 'https://middleware.blockchain.julasoft.com/';
-var URL = 'https://middleware.blockchain.julasoft.com/';
+let URL = 'http://localhost:8080/';
+//var URL = 'https://middleware.blockchain.julasoft.com/';
+
 var HEADERS = new Headers();
 HEADERS.append('Content-Type', 'application/json');
 HEADERS.append('Accept', 'application/json');
@@ -43,6 +44,7 @@ try {
     event.preventDefault();
     console.log('read_data');
     var respuesta = document.getElementById('respuesta');
+    respuesta.className = 'text-danger';
     respuesta.innerHTML = 'Consultando . '
     
     const valor = document.getElementById('number_id').value;
@@ -56,6 +58,7 @@ try {
     if (result.status === 200) {
         const datos = await result.text();
         console.log(datos)
+        respuesta.className = 'text-success';
         respuesta.innerHTML = datos
     } else if(result.status === 503){
         console.log('Error de autenticación');
@@ -76,18 +79,13 @@ try {
 
 
 const save_contract = async(event) => {
-//    body: JSON.stringify({
-//         title: 'foo',
-//         body: 'bar',
-//         userId: 1,
-//     }),
 try {
     event.preventDefault();
     console.log('save_contract');
     var respuesta = document.getElementById('respuesta');
-    respuesta.innerHTML = 'Consultando . '
+    respuesta.className = 'text-danger';
+    respuesta.innerHTML = 'Enviando ... '
 
-    //{"id":"87654321","lat":-34.61315,"lon":-58.37723,"state":"ok","valid":true}'
     contract_id = document.getElementById('contract_id').value;;
     final_insured = document.getElementById('final_insured').value;
     valid_since = document.getElementById('valid_since').value;
@@ -96,20 +94,19 @@ try {
     acceptance_date = document.getElementById('acceptance_date').value;
 
   
-    body = {"id":contract_id,
-           "lat":'-34.61315',
-           "lon":'-58.37723',
-           "state":"ok",
-           "valid":'true',
-           'contract' : {
-            'contract_id': contract_id,
-            'final_insured': final_insured,
-            'valid_since': valid_since,
-            'valid:until': valid_until,
-            'user_acepts':user_acepts,
-            'acceptance_date': acceptance_date,
-            
-           }
+    body = {"id"  : contract_id,
+            "lat" : "0",
+            "lon" : "0",
+            "state": "ok",
+            "valid": "true",
+            "contract" : {
+               "contract_id": contract_id,
+               "final_insured": final_insured,
+               "valid_since": valid_since,
+               "valid_until": valid_until,
+               "user_acepts": user_acepts,
+               "acceptance_date": acceptance_date,
+            }
            };
 
 
@@ -117,10 +114,11 @@ try {
     
 
     const result = await fetch(URL + '/data/save', {
-    method: 'POST',
-    headers: HEADERS,
-    body: JSON.stringify(body)
+        method: 'POST',
+        headers: HEADERS,
+        body: JSON.stringify(body)
     });
+    
 
     console.log(result);
     if (result.status === 200) {
@@ -128,14 +126,14 @@ try {
         console.log(datos)
         respuesta.innerHTML = datos
     } else if(result.status === 503){
-        console.log('Error de autenticación');
-        respuesta.innerHTML = 'Error de autenticación'
+        console.log('Error de autenticación.');
+        respuesta.innerHTML = 'Error de autenticación.'
     } else if(result.status === 400){
-        console.log('Dato no encontrado');
-        respuesta.innerHTML = 'Dato no encontrado'
+        console.log('Este contrato ya se encuentra registrado.');
+        respuesta.innerHTML = 'Este contrato ya se encuentra registrado.'
     } else {
-        console.log('Hubo un error desconocido');
-        respuesta.innerHTML = 'Hubo un error desconocido'    
+        console.log('Hubo un error desconocido.');
+        respuesta.innerHTML = 'Hubo un error desconocido.'    
     }
 
   } catch(error){
@@ -144,5 +142,3 @@ try {
   }
 };
 
-//blockchain_status();
-//read_data()
