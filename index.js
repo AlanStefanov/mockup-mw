e956f175d78697c7c1a27d665b02c0c04e735aba
felipemoralesquerol <felipe.morales.querol@gmail.com>
2022-08-15 23:10:50 -0300

    Fix mayor

--- index.js
+++ index.js
@@ -86,41 +86,58 @@ try {
     respuesta.className = 'text-danger';
     respuesta.innerHTML = 'Enviando ... '
 
-    contract_id = document.getElementById('contract_id').value;;
-    final_insured = document.getElementById('final_insured').value;
-    valid_since = document.getElementById('valid_since').value;
-    valid_until = document.getElementById('valid_since').value;
-    user_acepts = document.getElementById('user_acepts').value;
-    acceptance_date = document.getElementById('acceptance_date').value;
+    // Contratos
+    contract_contract_id = document.getElementById('contract_contract_id').value;
+    contract_final_insured = document.getElementById('contract_final_insured').value;
+    contract_valid_since = document.getElementById('contract_valid_since').value;
+    contract_valid_until = document.getElementById('contract_valid_since').value;
+    contract_user_accepts = document.getElementById('contract_user_accepts').value;
+    contract_acceptance_date = document.getElementById('contract_acceptance_date').value;
+    // Polizas
+    policy_policy_number = document.getElementById('policy_policy_number').value;
+    policy_policy_type = document.getElementById('policy_policy_type').value;
+    policy_valid_since = document.getElementById('policy_valid_since').value;
+    policy_final_insured = document.getElementById('policy_final_insured').value;
+    policy_source = document.getElementById('policy_source').value;
+    policy_destiny = document.getElementById('policy_destiny').value;
+    policy_condition = document.getElementById('policy_condition').value;
+    policy_conveyance = document.getElementById('policy_conveyance').value;
+    policy_container_type = document.getElementById('policy_container_type').value;
+    policy_merchandise = document.getElementById('policy_merchandise').value;
+    policy_insured_amount = document.getElementById('policy_insured_amount').value;
+    policy_currency = document.getElementById('policy_currency').value;
+    policy_reference_document = document.getElementById('policy_reference_document').value;
+
+
 
   
-    body = {"id"  : contract_id,
+    body = {"id"  : contract_contract_id,
             "lat" : "0",
             "lon" : "0",
             "state": "ok",
             "valid": "true",
             "contract" : {
-              "contract_id": contract_id,
-              "final_insured": final_insured,
-              "valid_since": valid_since,
-              "valid_until": valid_until,
-              "user_accepts": user_accepts,
-              "acceptance_date": acceptance_date, 
+              "contract_id": contract_contract_id,
+              "final_insured": contract_final_insured,
+              "valid_since": contract_valid_since,
+              "valid_until": contract_valid_until,
+              "user_accepts": contract_user_accepts,
+              "acceptance_date": contract_acceptance_date, 
             "policy": {
-                "policy_id": policy_id,
-                "policy_number":policy_number,
-                "policity_type":policity_type,
-                "valid_since":valid_since,
-                "final_insured":final_insured, 
-                "source":source,
-                "destiny":destiny, 
-                "condition":condition,
-                "conveyance":conveyance,
-                "container_type":container_type,
-                "merchandise": merchandise,
-                "insured_amount": insured_amount,
-                "currency": currency,
-                "reference_document":reference_document,
+                "policy_id": policy_policy_number,
+                "policy_number":policy_policy_number,
+                "policity_type":policy_policy_type,
+                "valid_since":policy_valid_since,
+                "final_insured":policy_final_insured, 
+                "source":policy_source,
+                "destiny":policy_destiny, 
+                "condition":policy_condition,
+                "conveyance":policy_conveyance,
+                "container_type":policy_container_type,
+                "merchandise": policy_merchandise,
+                "insured_amount": policy_insured_amount,
+                "currency": policy_currency,
+                "reference_document":policy_reference_document,
                 
             }
             }
@@ -141,6 +158,7 @@ try {
     if (result.status === 200) {
         const datos = await result.text();
         console.log(datos)
+        respuesta.className = 'text-success';
         respuesta.innerHTML = datos
     } else if(result.status === 503){
         console.log('Error de autenticación.');
