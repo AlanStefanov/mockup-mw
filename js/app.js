3b2e7def753f8b17abdeacf1b3cf026874fdecb5
AlanStefanov <alan.emanuel.stefanov@gmail.com>
2022-08-17 13:48:00 -0300

    add functions

--- js/app.js
+++ js/app.js
@@ -78,19 +78,16 @@ try {
 };
 
 const save_contract = async(event) => {
-//    body: JSON.stringify({
-//         title: 'foo',
-//         body: 'bar',
-//         userId: 1,
-//     }),
+
 try {
     event.preventDefault();
     console.log('save_contract');
     var respuesta = document.getElementById('respuesta');
-    respuesta.innerHTML = 'Consultando . '
+    respuesta.innerHTML = 'Enviando . '
 
-    //{"id":"87654321","lat":-34.61315,"lon":-58.37723,"state":"ok","valid":true}'
+
     contract_contract_id = document.getElementById('contract_contract_id').value;
+    contract_contract_number = document.getElementById('contract_contract_number').value;
     contract_final_insured = document.getElementById('contract_final_insured').value;
     contract_valid_since = document.getElementById('contract_valid_since').value;
     contract_valid_until = document.getElementById('contract_valid_until').value;
@@ -99,12 +96,12 @@ try {
 
   
     body = {"id"  : contract_contract_id,
-            "lat" : "0",
-            "lon" : "0",
-            "state": "ok",
-            "valid": "true",
+            // "lat" : "0",
+            // "lon" : "0",
+            // "state": "ok",
+            // "valid": "true",
             "contract" : {
-              "contract_id": contract_contract_id,
+              "contract_number": contract_contract_number,
               "final_insured": contract_final_insured,
               "valid_since": contract_valid_since,
               "valid_until": contract_valid_until,
@@ -180,10 +177,10 @@ try {
 
   
     body = {"id"  : contract_contract_id,
-            "lat" : "0",
-            "lon" : "0",
-            "state": "ok",
-            "valid": "true",
+            // "lat" : "0",
+            // "lon" : "0",
+            // "state": "ok",
+            // "valid": "true",
             "contract" : {
               "contract_id": contract_contract_id,
               "final_insured": contract_final_insured,
@@ -245,7 +242,9 @@ try {
   }
 };
 
-const save_policy = async(event) => {
+
+
+const save_policy = async (event) => {
   try {
       event.preventDefault();
       console.log('save_policy');
@@ -254,12 +253,16 @@ const save_policy = async(event) => {
       respuesta.innerHTML = 'Enviando ... '
   
       // Contratos
-      policy_contract_id = document.getElementById('policy_contract_id').value;
+      policy_policy_id = document.getElementById('policy_policy_id').value;
     
       // Polizas
+      policy_contract_number = document.getElementById('policy_contract_number').value;
       policy_policy_number = document.getElementById('policy_policy_number').value;
       policy_policy_type = document.getElementById('policy_policy_type').value;
       policy_valid_since = document.getElementById('policy_valid_since').value;
+      policy_valid_since_hour = document.getElementById('policy_valid_since_hour').value;
+      policy_valid_until = document.getElementById('policy_valid_until').value;
+      policy_valid_until_hour = document.getElementById('policy_valid_until_hour').value;
       policy_final_insured = document.getElementById('policy_final_insured').value;
       policy_source = document.getElementById('policy_source').value;
       policy_destiny = document.getElementById('policy_destiny').value;
@@ -274,19 +277,22 @@ const save_policy = async(event) => {
   
   
     
-      body = {"id"  : policy_contract_id,
-              "lat" : "0",
-              "lon" : "0",
-              "state": "ok",
-              "valid": "true",
-              "contract" : {
-                "contract_id": policy_contract_id,
+      body = {"id"  : policy_policy_id,
+              // "lat" : "0",
+              // "lon" : "0",
+              // "state": "ok",
+              // "valid": "true",
+              "contract": {
+              "contract_number": policy_contract_number,
                 
               "policy": {
                   "policy_id": policy_policy_number,
                   "policy_number":policy_policy_number,
                   "policity_type":policy_policy_type,
-                  "valid_since":policy_valid_since,
+                "valid_since": policy_valid_since,
+                "valid_since_hour": policy_valid_since_hour, 
+                "valid_until": policy_valid_until,        
+                "valid_until_hour": policy_valid_until_hour,
                   "final_insured":policy_final_insured, 
                   "source":policy_source,
                   "destiny":policy_destiny, 
@@ -337,6 +343,109 @@ const save_policy = async(event) => {
   };
   
 
+  const save_endorsement = async (event) => {
+    try {
+        event.preventDefault();
+        console.log('save_endorsement');
+        var respuesta = document.getElementById('respuesta');
+        respuesta.className = 'text-danger';
+        respuesta.innerHTML = 'Enviando ... '
+    
+        //ID
+        endorsement_endorsement_id = document.getElementById('endorsement_endorsement_id').value;
+      
+        // Endoso
+      policy_contract_number = document.getElementById('policy_contract_number').value;
+      policy_policy_number = document.getElementById('policy_policy_number').value;
+      policy_policy_type = document.getElementById('policy_policy_type').value;
+      policy_valid_since = document.getElementById('policy_valid_since').value;
+      policy_valid_since_hour = document.getElementById('policy_valid_since_hour').value;
+      policy_valid_until = document.getElementById('policy_valid_until').value;
+      policy_valid_until_hour = document.getElementById('policy_valid_until_hour').value;
+      policy_final_insured = document.getElementById('policy_final_insured').value;
+      policy_source = document.getElementById('policy_source').value;
+      policy_destiny = document.getElementById('policy_destiny').value;
+      policy_condition = document.getElementById('policy_condition').value;
+      policy_conveyance = document.getElementById('policy_conveyance').value;
+      policy_container_type = document.getElementById('policy_container_type').value;
+      policy_merchandise = document.getElementById('policy_merchandise').value;
+      policy_insured_amount = document.getElementById('policy_insured_amount').value;
+      policy_currency = document.getElementById('policy_currency').value;
+      policy_reference_document = document.getElementById('policy_reference_document').value;
+    
+    
+    
+      
+        body = {"id"  : endorsement_endorsement_id,
+                // "lat" : "0",
+                // "lon" : "0",
+                // "state": "ok",
+                // "valid": "true",
+                "contract": {
+                "contract_number": policy_contract_number,
+                  
+                "policy": {
+                  "policy_id": policy_policy_number,
+                  "policy_number":policy_policy_number,
+                  "policity_type":policy_policy_type,
+                "valid_since": policy_valid_since,
+                "valid_since_hour": policy_valid_since_hour, 
+                "valid_until": policy_valid_until,        
+                "valid_until_hour": policy_valid_until_hour,
+                  "final_insured":policy_final_insured, 
+                  "source":policy_source,
+                  "destiny":policy_destiny, 
+                  "condition":policy_condition,
+                  "conveyance":policy_conveyance,
+                  "container_type":policy_container_type,
+                  "merchandise": policy_merchandise,
+                  "insured_amount": policy_insured_amount,
+                  "currency": policy_currency,
+                  "reference_document":policy_reference_document,
+                  
+              }
+              }
+            };
+  
+    
+        console.log(body)
+        
+    
+        const result = await fetch(URL + '/data/save', {
+            method: 'POST',
+            headers: HEADERS,
+            body: JSON.stringify(body)
+        });
+        
+    
+        console.log(result);
+        if (result.status === 200) {
+            const datos = await result.text();
+            console.log(datos)
+            respuesta.className = 'text-success';
+            respuesta.innerHTML = datos
+        } else if(result.status === 503){
+            console.log('Error de autenticación.');
+            respuesta.innerHTML = 'Error de autenticación.'
+        } else if(result.status === 400){
+            console.log('Este contrato ya se encuentra registrado.');
+            respuesta.innerHTML = 'Este contrato ya se encuentra registrado.'
+        } else {
+            console.log('Hubo un error desconocido.');
+            respuesta.innerHTML = 'Hubo un error desconocido.'    
+        }
+    
+      } catch(error){
+        console.log(error);
+        respuesta.innerHTML = 'El server Middleware BlockChain no responde. [' + error + ']';
+      }
+    };
+    
+
+
+
+  
+  //Siniestros
   const save_claims = async(event) => {
     try {
         event.preventDefault();
@@ -345,37 +454,43 @@ const save_policy = async(event) => {
         respuesta.className = 'text-danger';
         respuesta.innerHTML = 'Enviando ... '
     
-        // Contratos
-        claims_contract_id = document.getElementById('claims_contract_id').value;
+        //  ID
+        claims_claims_id = document.getElementById('claims_claims_id').value;
+      
+        // Contrato
+        claims_contract_number = document.getElementById('claims_contract_number').value;
       
         // Polizas
         claims_policy_number = document.getElementById('claims_policy_number').value;
 
         // Siniestros
+        claims_claim_number = document.getElementById('claims_claim_number').value;
         claims_date_complaint = document.getElementById('claims_date_complaint').value;
         claims_date_claim = document.getElementById('claims_date_claim').value;
         claims_type_claim = document.getElementById('claims_type_claim').value;
         claims_crash_site = document.getElementById('claims_crash_site').value;
+        claims_crash_hour = document.getElementsByClassName('claims_crash_hour').value;
         claims_reclaimed_amount = document.getElementById('claims_reclaimed_amount').value;
         claims_currency = document.getElementById('claims_currency').value;
         claims_claimed_status = document.getElementById('claims_claimed_status').value;
   
-        body = {"id"  : claims_contract_id,
-                "lat" : "0",
-                "lon" : "0",
-                "state": "ok",
-                "valid": "true",
+        body = {"id"  : claims_claims_id,
+                // "lat" : "0",
+                // "lon" : "0",
+                // "state": "ok",
+                // "valid": "true",
                 "contract" : {
-                  "contract_id": claims_contract_id,
+                  "contract_id": claims_contract_number,
                   
                 "policy": {
                     "policy_id": claims_policy_number,
                   
-                "claims":{
-                    "date_complaint": claims_date_complaint,                   
+                  "claims": {
+                    "claim_number": claims_claim_number,                                     
                     "date_claim": claims_date_claim,
                     "type_claim": claims_type_claim,
                     "crash_site": claims_crash_site, 
+                    "crash_hour": claims_crash_hour,
                     "reclaimed_amount": claims_reclaimed_amount,
                     "currency": claims_currency,
                     "claimed_status": claims_claimed_status,            
@@ -386,6 +501,7 @@ const save_policy = async(event) => {
                 }
               };
     
+      
     
         console.log(body)
         
---
+++ js/misc.js
@@ -0,0 +1,44 @@
+//ID contrato
+function cambioContrato(str) {
+  console.log('cambioContrato');
+  var elem = document.getElementById('contract_contract_id');
+  auxC = document.getElementById('contract_contract_number').value;
+  elem.value = auxC != '' ?  "C" + auxC : '';
+}
+    
+// ID poliza
+function cambioPoliza(str) {
+    console.log('cambioPoliza');
+  var elem = document.getElementById('policy_policy_id');
+  p_contract = document.getElementById('policy_contract_number').value;
+  p_policy = document.getElementById('policy_policy_number').value;
+
+  elem.value = "C" + p_contract + "P" +  p_policy;
+    
+}
+
+// ID endoso
+function cambioEndoso(str) {
+    console.log('cambioEndoso');
+    var elem = document.getElementById('endorsement_endorsement_id');
+    e_contract = document.getElementById('policy_contract_number').value;
+    e_policy = document.getElementById('policy_policy_number').value;
+    e_endorsement = document.getElementById('policy_endorsement_number').value; 
+
+  elem.value = "C" + e_contract + "P" + e_policy + "E" + e_endorsement;
+}
+
+// ID siniestro
+function cambioSiniestro(str) {
+  console.log('cambioSiniestro');
+  var elem = document.getElementById('claims_claims_id');
+  s_contract = document.getElementById('claims_contract_number').value;
+  s_policy = document.getElementById('claims_policy_number').value;
+  s_claim = document.getElementById('claims_claim_number').value;
+  elem.value = "C" + s_contract + "P" + s_policy + "E" + s_claim;
+  
+
+}
+
+
+
